bc67470704357259eaaa70bbce55af3594450c74
momentino <filippomomente@gmail.com>
2025-02-26 16:04:45 +0100

    chore: cleaned code and made utils into a package

--- agents/__init__.py
+++ agents/__init__.py
@@ -23,23 +23,3 @@ def build_agent_list(game: GameBenchmark, agent_kwargs: str, gen_kwargs: str) ->
         agent = agents[-1]
         agents.extend([agent for _ in range(min_num_agents - len(agents))])
     return agents
-
-
-
-
-
-def create_agents(agent_args_list: List[str], agents_root: str, game: GameBenchmark) -> List[Agent]:
-    agents = [init_agent_from_args(agent_args, agents_root) for agent_args in agent_args_list]
-    # TODO: when we have the same model playing multiple agents, we could have one single instance in the memory rather than two, and have separate histories
-    min_num_agents = 1
-    max_num_agents = 1 if game.is_single_player() else 2
-    if len(agents) > max_num_agents:
-        message = f"Too many agents for this game. The maximum number of player agents for this game is {max_num_agents}"
-        stdout_logger.error(message)
-        raise ValueError(message)
-    elif len(agents) < min_num_agents:
-        message = f"The number of agents was insufficient for playing the game. Creating {min_num_agents - len(agents)} agents with the last model specified in the arguments."
-        stdout_logger.warning(message)
-        agent = agents[-1]
-        agents.extend([agent for _ in range(min_num_agents - len(agents))])
-    return agents
