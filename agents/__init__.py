4959eb5c997772084a65212787809c067a7382cd
momentino <filippomomente@gmail.com>
2025-02-26 11:48:05 +0100

    fix: logger

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,6 +2,7 @@ from typing import List
 from agents.hf_agent import HFAgent
 from playpen.agents.base_agent import Agent
 from playpen.clemgame.clemgame import GameBenchmark
+from utils.logger import out_logger
 
 def build_agent_list(game: GameBenchmark, agent_kwargs: str, gen_kwargs: str) -> List[Agent]:
 
@@ -14,11 +15,11 @@ def build_agent_list(game: GameBenchmark, agent_kwargs: str, gen_kwargs: str) ->
     max_num_agents = 1 if game.is_single_player() else 2
     if len(agents) > max_num_agents:
         message = f"Too many agents for this game. The maximum number of player agents for this game is {max_num_agents}"
-        stdout_logger.error(message)
+        out_logger.error(message)
         raise ValueError(message)
     elif len(agents) < min_num_agents:
         message = f"The number of agents was insufficient for playing the game. Creating {min_num_agents - len(agents)} agents with the last model specified in the arguments."
-        stdout_logger.warning(message)
+        out_logger.warning(message)
         agent = agents[-1]
         agents.extend([agent for _ in range(min_num_agents - len(agents))])
     return agents
